9896a8330b7ca95e0be4266b6334e840a845d58d
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-06 15:27:52 -0500

    copmlete lap

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+const employee = {
+    name: 'Maher Gad',
+    streetAddress: '243 Kennedy Blvd',
+  };
+  
